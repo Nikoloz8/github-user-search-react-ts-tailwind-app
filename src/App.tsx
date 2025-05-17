021ea472be34b05839c2afcc0ed4773b7c8adb88
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-17 18:01:53 +0400

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -94,9 +94,8 @@ function App() {
         <div className='w-[800px] flex flex-col gap-[30px]'>
           <AppHeader textPreset1={textPreset1} theme={theme} sunSVG={sunSVG} moonSVG={moonSVG} setTheme={setTheme} />
           <AppSearch theme={theme} name={name} isMobile={isMobile} setName={setName} gitFetch={gitFetch} textPreset1={textPreset1} notFound={notFound} />
-          <div className={`p-[50px]! max-sm:gap-[50px] max-sm:flex-col max-sm:items-center max-sm:justify-center w-[100%] bg-[#1E2A47] flex rounded-[15px] ${!theme ? "bg-[#FEFEFE]! shadow-[0_16px_30px_-10px_#4660BB33]" : ""}`}>
+          <div className={`p-[50px]! max-sm:gap-[50px] max-sm:p-[30px]! max-sm:flex-col max-sm:items-center max-sm:justify-center w-[100%] bg-[#1E2A47] flex rounded-[15px] ${!theme ? "bg-[#FEFEFE]! shadow-[0_16px_30px_-10px_#4660BB33]" : ""}`}>
             <img className={`w-[120px] max-sm:m-0! h-[120px] mr-[50px] rounded-[100%]`} src={info.imageUrl ? info.imageUrl : undefined} alt="" />
-            
             <div className='w-[100%] flex max-sm:items-center max-sm:justify-center  flex-col gap-[30px]'>
               <AppInfoNameBioDate info={info} textPreset1={textPreset1} theme={theme} textPreset2={textPreset2}/>   
               <AppInfoGeneralStats theme={theme} textPreset1={textPreset1} textPreset2={textPreset2} info={info}/>
